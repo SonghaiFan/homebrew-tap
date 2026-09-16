ea4c154cafa8ea12d0625f9d870bc07bb682a52a
Songhai <95398822+SonghaiFan@users.noreply.github.com>
2026-09-16 14:31:07 +1000

    Add LeftOpen cask v0.2.0

---
+++ Casks/leftopen.rb
@@ -0,0 +1,17 @@
+cask "leftopen" do
+  version "0.2.0"
+  sha256 "601f26b90ede20060b73b10eabc56f586902e6b9808c010b394a7a4dc3096af4"
+
+  url "https://github.com/SonghaiFan/leftopen/releases/download/v#{version}/LeftOpen.zip"
+  name "LeftOpen"
+  desc "See what your tools left running on localhost"
+  homepage "https://github.com/SonghaiFan/leftopen"
+
+  depends_on macos: ">= :sonoma"
+
+  app "LeftOpen.app"
+
+  zap trash: [
+    "~/Library/Saved Application State/app.leftopen.mac.savedState",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,22 @@
+# Homebrew Tap for SonghaiFan
+
+Official Homebrew tap for LeftOpen and related utilities.
+
+## How to Install LeftOpen
+
+```bash
+brew install --cask SonghaiFan/tap/leftopen
+```
+
+Or add the tap first:
+
+```bash
+brew tap SonghaiFan/tap
+brew install --cask leftopen
+```
+
+## Updating
+
+```bash
+brew upgrade --cask leftopen
+```
