4b8bf91ef2d6244cd57f87c7f122553acbad65c0
Songhai <95398822+SonghaiFan@users.noreply.github.com>
2026-09-16 19:04:40 +1000

    leftopen 0.2.2: native CLI binary support

--- Casks/leftopen.rb
+++ Casks/leftopen.rb
@@ -1,6 +1,6 @@
 cask "leftopen" do
-  version "0.2.1"
-  sha256 "4d015968e2b747871c6a8383d86398d5ecad871a7a9c1b7971a64e30f3eda871"
+  version "0.2.2"
+  sha256 "30c8126238257769c14e84409542ecd94fc74de448bf2be26db8436577d9c4f4"
 
   url "https://github.com/SonghaiFan/leftopen/releases/download/v#{version}/LeftOpen.zip"
   name "LeftOpen"
@@ -10,6 +10,7 @@ cask "leftopen" do
   depends_on macos: :sonoma
 
   app "LeftOpen.app"
+  binary "#{appdir}/LeftOpen.app/Contents/MacOS/leftopen"
 
   zap trash: [
     "~/Library/Saved Application State/app.leftopen.mac.savedState",
