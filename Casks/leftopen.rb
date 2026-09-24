3e13b5609f0d9271ed9436d72a582a8c80c3f48c
Songhai <95398822+SonghaiFan@users.noreply.github.com>
2026-09-24 19:54:07 +1000

    leftopen 0.3.3: release update

--- Casks/leftopen.rb
+++ Casks/leftopen.rb
@@ -1,6 +1,6 @@
 cask "leftopen" do
-  version "0.3.2"
-  sha256 "5e2ba2eb2f7e8f190f722890e99e621a23b638e1b596cae60ff20a6e49ce7810"
+  version "0.3.3"
+  sha256 "5ddbcd709a27aa3018a0acd39e8db9569c5e1226e1027dbd3991ab59c1e31d1e"
 
   url "https://github.com/SonghaiFan/leftopen/releases/download/v#{version}/LeftOpen.zip"
   name "LeftOpen"
